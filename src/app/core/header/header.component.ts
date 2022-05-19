79c5177581659043e4e243f5337bfdfd292e0098
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-05-19 21:14:51 +0100

    Creating a part of header2

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild('searchExpanded') searchExpanded!: ElementRef;
+  @ViewChild('modal')  modal!: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  showModal() {
+    this.searchExpanded.nativeElement.style.display='block';
+    this.modal.nativeElement.style.display='block';
+  }
+
+  closeModal() {
+    this.searchExpanded.nativeElement.style.display='none';
+    this.modal.nativeElement.style.display='none';
+  }
 }
