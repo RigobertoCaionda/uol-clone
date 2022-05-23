f8185e362d3b6b4b3d3a949ffd609ee373a969fa
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-05-23 16:59:10 +0100

    Creating a part of home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { FooterComponent } from './shared/layouts/footer/footer.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
