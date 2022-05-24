2fdd23adf6de71b4a0cbe0d144112d3bfeeb9786
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2022-05-24 16:22:24 +0100

    Adding home things

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  // Para testar o ngFor apenas
+  items = Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  // Para testar o ngFor apenas
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
